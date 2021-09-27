9e411849e784f254adcd1a3f4e185929f0b01dd5
Your Name <thiagohpr@al.insper.edu.br>
2021-09-27 16:47:41 -0300

    Editar e postgres

--- notes/views.py
+++ notes/views.py
@@ -4,13 +4,20 @@ from .models import Note
 
 def index(request):
     if request.method == 'POST':
-        title = request.POST.get('titulo')
-        content = request.POST.get('detalhes')
-        # Note.objects.create(title=title,content=content)
-        note=Note()
-        note.title=title
-        note.content=content
-        note.save()
+        tit = request.POST.get('titulo')
+        if len(Note.objects.all().filter(title=tit))==0:
+            content = request.POST.get('detalhes')
+            # Note.objects.create(title=title,content=content)
+            note=Note()
+            note.title=tit
+            note.content=content
+            note.save()
+        else:
+            note=Note.objects.get(title=tit)
+            note.title=request.POST.get('titulo')
+            note.content=request.POST.get('detalhes')
+            note.save()
+    
 
         return redirect('index')
     else:
@@ -22,6 +29,7 @@ def delete(request,note_id):
     note=Note.objects.get(id=note_id)
     note.delete()
     return redirect('index')
+    
 # def update(request,note_id):
 #     note_id=int(note_id)
 #     note=Note.objects.get(id=note_id)
